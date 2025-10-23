5a75197787a51293e52f7a9c1c5bd8592d4af14b
Piyamon-cp <piyamon.chuenpaew@gmail.com>
2025-10-23 19:48:17 +0700

    Update Confusion Matric

--- src/train_models.py
+++ src/train_models.py
@@ -5,12 +5,8 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 # Funcrion to train model
 from sklearn.model_selection import train_test_split
-
-# Function to evaluate F1 Score
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
+
 
 data_dict = pickle.load(open("./data_75pics.pickle", "rb"))
 
@@ -37,55 +33,3 @@ f.close()
 print("Model trained and saved to model.p")
 # print(data_dict.keys())
 # print(data_dict)
-
-
-# # Accuracy Score
-# # Accuracy on training data
-# x_train_prediction = model.predict(x_train)
-# training_data_accuracy = accuracy_score(y_train, x_train_prediction)
-# print('Accuracy on Training data : ', round(training_data_accuracy*100,2),'%')
-# # Accuracy on test data
-# x_test_prediction = model.predict(x_test)
-# test_data_accuracy = accuracy_score(y_test,x_test_prediction)
-# print('Accuracy on Test data : ', round(test_data_accuracy*100,2),'%')
-
-# # Precision
-# # precision for training data predictions
-# precision_train = precision_score(y_train, x_train_prediction, average='weighted')
-# print('Training data Precision = ', precision_train)
-# # precision for test data predictions
-# precision_test = precision_score(y_test, x_test_prediction, average='weighted')
-# print('Test data Precision = ', precision_test)
-
-# # Recall
-# # recall for training data predictions
-# recall_train = recall_score(y_train, x_train_prediction, average='weighted')
-# print('Training data Recall = ', recall_train)
-# # recall for test data predictions
-# recall_test = recall_score(y_test, x_test_prediction, average='weighted')
-# print('Test data Recall = ', recall_test)
-
-# # F1
-# # f1 score for training data predictions
-# f1_score_train = f1_score(y_train, x_train_prediction, average='weighted')
-# print('Training data F1 Score = ', f1_score_train)
-# # f1 score for test data predictions
-# f1_score_test = f1_score(y_test, x_test_prediction, average='weighted')
-# print('Test data F1 Score = ', f1_score_test)
-
-
-# # Precision Recall and F1 function
-# def precision_recall_f1_score(true_labels, pred_labels):
-#     precision_value = precision_score(true_labels, pred_labels, average='weighted')
-#     recall_value = recall_score(true_labels, pred_labels, average='weighted')
-#     f1_score_value = f1_score(true_labels, pred_labels, average='weighted')
-
-#     print('Precision =', precision_value)
-#     print('Recall =', recall_value)
-#     print('F1 Score =', f1_score_value)
-
-# print('---------- Classification metrics for training data ------------')
-# precision_recall_f1_score(y_train, x_train_prediction)
-
-# print('---------- Classification metrics for test data ----------------')
-# precision_recall_f1_score(y_test, x_test_prediction)
